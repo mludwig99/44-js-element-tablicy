a80c6d59735f3ea6099e5b0f54f2108e86c55a94
mludwig99 <michalludwig99@gmail.com>
2022-06-11 22:35:45 +0200

    44-element-tablicy-poprawione

--- script/main.js
+++ script/main.js
@@ -1,14 +1,16 @@
-let tab = [4, 5, 6, 9, 3];
+let tab = [7, 5, 6, 9, 3];
 
-function returnNr1() {
-    console.log(tab[0])
+function returnNr1(a) {
+    console.log(a[0]);
+     return a
 }
 
-function returnNr2(tab) {
-    let firtsNumber = console.log(tab[0]);
+function returnNr2(b) {
+    let firtsNumber = b[0];
     return firtsNumber;
 }
 
 
-returnNr1();
-returnNr2(tab);
+returnNr1([2, 5, 6, 9, 1]);
+returnNr1(tab)
+console.log(returnNr2(tab))
